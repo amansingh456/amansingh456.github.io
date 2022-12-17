03ca346e4bab6eb076b3beaaf6a99d3b3967ff94
Aman Singh Rajawat <110046883+amansingh456@users.noreply.github.com>
2022-12-17 15:45:19 +0530

    some changes have done

--- src/Components/Theme.jsx
+++ src/Components/Theme.jsx
@@ -2,7 +2,7 @@ export const Theme = {
   colors: {
     base1: "#2e2d2e", // deep green backgrounf [navbar, about , contact]
     base2: "#fff", //white
-    primary: "#e3a408", //lighter green
+    primary: "#fe9401", //lighter green
     primary1: "#343334", // deepest green [portfolio, footer]
   },
 };

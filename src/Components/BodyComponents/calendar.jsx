90470f95dae6c77e77f5cdc3d9bc1841b138a360
Aman Singh Rajawat <110046883+amansingh456@users.noreply.github.com>
2022-12-17 22:24:37 +0530

    some chnges done

--- src/Components/BodyComponents/calendar.jsx
+++ src/Components/BodyComponents/calendar.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactGitHubCalendar from 'react-ts-github-calendar';
+
 import { Theme } from "../Theme";
 import ScrollAnimation from "react-animate-on-scroll";
 import { RenderSectionHeading } from "../common/commonComponent";
 import { Box, Grid } from '@material-ui/core';
 
 const Calendar = () => {
+   
     return (
         <>
-            <Box style={{ backgroundColor: Theme.colors.base1, padding: "0 15px", paddingBottom: "50px", paddingTop: "50px" }}>
+            <Box style={{ backgroundColor: Theme.colors.base1, padding: "0 0px", paddingBottom: "50px", paddingTop: "50px" }}>
                 <ScrollAnimation animateIn='fadeIn'>
                     <Grid
                         container
@@ -25,8 +27,8 @@ const Calendar = () => {
                             //   "Got my Portfolio seen , than feel free to contact me for your future projects",
                         })}
                         </Grid>
-                        <Box className='calendar' style={{ color: "#ffffff" }}>
-                            <ReactGitHubCalendar userName="amansingh456" responsive="true" style={{ color: "#ffffff" }} />
+                        <Box className='calendar callu' style={{ color: "#ffffff" }}>
+                            <ReactGitHubCalendar global_stats={true} tooltips  responsive userName="amansingh456"  style={{ color: "#ffffff" }} />
                         </Box>
                 </ScrollAnimation>
             </Box>

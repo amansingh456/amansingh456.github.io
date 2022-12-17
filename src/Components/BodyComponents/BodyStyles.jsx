3dd84a594625e22daa58e66be886d405388abb9f
Aman Singh Rajawat <110046883+amansingh456@users.noreply.github.com>
2022-12-17 15:45:29 +0530

    some changes have done

--- src/Components/BodyComponents/BodyStyles.jsx
+++ src/Components/BodyComponents/BodyStyles.jsx
@@ -8,7 +8,7 @@ export const useStyles = makeStyles((theme) => ({
   },
   sectionDark: {
     backgroundColor: Theme.colors.primary1,
-    padding: theme.spacing(10, 0, 8, 0),
+    padding: theme.spacing(5, 0, 8, 0),
   },
 
   //common
